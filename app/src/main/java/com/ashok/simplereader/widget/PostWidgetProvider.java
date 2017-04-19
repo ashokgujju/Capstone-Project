75931ad3deb2a621cf0ecb75749e2f85b8280cd1
ashok <ashokg.geek@gmail.com>
2017-04-19 15:27:37 +0530

    implement posts widget

--- app/src/main/java/com/ashok/simplereader/widget/PostWidgetProvider.java
+++ app/src/main/java/com/ashok/simplereader/widget/PostWidgetProvider.java
@@ -1,10 +1,52 @@
 package com.ashok.simplereader.widget;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.TaskStackBuilder;
+import android.widget.RemoteViews;
+
+import com.ashok.simplereader.R;
+import com.ashok.simplereader.ui.PostDetailActivity;
+import com.ashok.simplereader.ui.PostListActivity;
 
 /**
  * Created by ashok on 3/4/17.
  */
 
 public class PostWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.posts_widget);
+
+            Intent intent = new Intent(context, PostListActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.title, pendingIntent);
+
+            views.setRemoteAdapter(R.id.list,
+                    new Intent(context, PostWidgetRemoteViewService.class));
+
+            PendingIntent clickPendingIntent = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(new Intent(context, PostDetailActivity.class))
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setPendingIntentTemplate(R.id.list, clickPendingIntent);
+
+            views.setEmptyView(R.id.list, R.id.widget_empty);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        if (intent.getAction().equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
+            AppWidgetManager widgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = widgetManager.getAppWidgetIds(new ComponentName(context, getClass()));
+            widgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.list);
+        }
+    }
 }
--- app/src/main/java/com/ashok/simplereader/widget/PostWidgetRemoteViewService.java
+++ app/src/main/java/com/ashok/simplereader/widget/PostWidgetRemoteViewService.java
@@ -1,15 +1,120 @@
 package com.ashok.simplereader.widget;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.Binder;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import com.ashok.simplereader.R;
+import com.ashok.simplereader.data.PostColumns;
+import com.ashok.simplereader.data.PostProvider;
+import com.ashok.simplereader.ui.PostDetailActivity;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.dean.jraw.models.Submission;
+
+import java.io.IOException;
 
 /**
  * Created by ashok on 3/4/17.
  */
 
 public class PostWidgetRemoteViewService extends RemoteViewsService {
+    private Context context;
+
+    public PostWidgetRemoteViewService() {
+        this.context = this;
+    }
+
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return null;
+        return new RemoteViewsFactory() {
+
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+
+
+                final long identityToken = Binder.clearCallingIdentity();
+
+                data = getContentResolver().query(PostProvider.Posts.CONTENT_URI,
+                        null, null, null, null);
+
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position))
+                    return null;
+
+                RemoteViews views = new RemoteViews(getPackageName(), R.layout.item_post_widget_list);
+
+                try {
+                    Submission post = new Submission(new ObjectMapper()
+                            .readTree(data.getString(data.getColumnIndex(PostColumns.DATA))));
+
+                    views.setTextViewText(R.id.title, post.getTitle());
+                    views.setTextViewText(R.id.subreddit, post.data("subreddit_name_prefixed"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Intent fillIntent = new Intent();
+                fillIntent.putExtra(PostDetailActivity.POST_JSON,
+                        data.getString(data.getColumnIndex(PostColumns.DATA)));
+                views.setOnClickFillInIntent(R.id.list_item, fillIntent);
+
+                return views;
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.item_post_widget_list);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                if (data.moveToPosition(i))
+                    return data.getLong(data.getColumnIndex(PostColumns._ID));
+                return i;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
     }
 }
